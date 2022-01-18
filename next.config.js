e09019a1e7b5772a7172f35b44e21d58b78ea66d
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-01-18 13:39:25 -0300

    add analyze script

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@ const withMDX = require('@next/mdx')({
 	extension: /\.mdx$/,
 })
 
-module.exports = withMDX({
-	pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
-	swcMinify: true,
-	images: {
-		formats: ['image/avif', 'image/webp'],
-	},
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withBundleAnalyzer(
+	withMDX({
+		pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+		swcMinify: true,
+		images: {
+			formats: ['image/avif', 'image/webp'],
+		},
+	}),
+)
