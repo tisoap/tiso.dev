c984eeb847c262bafb3089f2bc738f780836df53
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-02-13 11:50:53 -0300

    Update linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,15 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path')
-const tsConfigPath = path.resolve(__dirname, 'tsconfig.json')
-
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
 	parserOptions: {
-		ecmaVersion: 'latest',
-		sourceType: 'module',
-		ecmaFeatures: {
-			impliedStrict: true,
-			jsx: true,
-		},
-		tsconfigRootDir: __dirname,
-		project: [tsConfigPath],
+		tsconfigRootDir: '.',
+		project: ['./tsconfig.json'],
 	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
-		'import/resolver': {
-			node: {
-				paths: ['.'],
-			},
-		},
-	},
-	env: {
-		browser: true,
-		amd: true,
-		node: true,
-	},
-	plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier', 'import'],
+	plugins: ['prettier'],
 	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:jsx-a11y/recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		'plugin:prettier/recommended',
+		'@tisoap/eslint-config-ts-react',
 		'plugin:@next/next/recommended',
-		'plugin:import/recommended',
-		'plugin:import/typescript',
+		'plugin:prettier/recommended',
 	],
 	rules: {
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'error',
 		'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-		'react/prop-types': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'@typescript-eslint/consistent-type-imports': 'error',
-		'import/order': [
-			'error',
-			{
-				groups: [
-					'builtin',
-					'external',
-					'internal',
-					'parent',
-					'sibling',
-					'index',
-					'type',
-					'object',
-					'unknown',
-				],
-				'newlines-between': 'never',
-				alphabetize: {
-					order: 'asc',
-					caseInsensitive: false,
-				},
-			},
-		],
 	},
 }
